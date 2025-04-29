4d26c5db1897406bfe205a57342585da77fe0981
Dominic Polsinelli <32404918+DomPulse@users.noreply.github.com>
2025-04-28 23:43:38 -0500

    Add files via upload
    damn this is slow, kinda crashes python actually

--- bam_test.py
+++ bam_test.py
@@ -1,4 +1,19 @@
 import bamnostic as bs
+
 test_file_path = r'F:\Big_MET_data\morpho_trans_extracted\ar_657938511_STAR_Aligned.sortedByCoord.out.bam'
+
 bam = bs.AlignmentFile(test_file_path, 'rb')
-print(bam.header)
+
+count_dracula = {}
+
+read_count = 0
+for read in bam:
+	the_whole_tomale = f"{read.reference_name}-{read.reference_start}"
+	print(the_whole_tomale)
+	if (the_whole_tomale not in count_dracula):
+		count_dracula[the_whole_tomale] = 1
+	else:
+		count_dracula[the_whole_tomale] += 1
+
+print(count_dracula)
+
