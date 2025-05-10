2df127567fcd5fd6f6266b4fa5b78988b02ca2ab
Dominic Polsinelli <32404918+DomPulse@users.noreply.github.com>
2025-05-09 21:54:57 -0500

    Add files via upload
    Just leaving this here as a last stop before I make it work for all output features

--- input_to_ephys_correlate_check.py
+++ input_to_ephys_correlate_check.py
@@ -7,12 +7,50 @@ import classify_patch_clamp as cpc
 import gc
 from sklearn.feature_selection import mutual_info_regression as mir
 
+counts_path = r"F:\Big_MET_data\trans_data_cpm\20200513_Mouse_PatchSeq_Release_cpm.v2.csv"
 just_trans_path = 'F:\Big_MET_data\metadata_pluse_trans.csv'
 mopho_trans_path = 'F:\Big_MET_data\combo_morph_trans.csv'
 ephys_path = r'F:\Big_MET_data\derived_ephys'
 morpho_trans_data = pd.read_csv(mopho_trans_path)
-
 currents_to_check = [-0.11, -0.09, -0.07, -0.05, -0.03, 0.05, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13, 0.15, 0.17, 0.19, 0.21, 0.23, 0.25]
+
+def norm_col(array):
+	min_by_col = np.min(array, axis = 0) 
+	array = np.subtract(array, min_by_col)
+	max_by_col = np.max(array, axis = 0)
+	return np.divide(array, max_by_col)
+
+def norm_row(array):
+	min_by_row = np.min(array, axis = 1) 
+	min_by_row = min_by_row.reshape(-1,1)
+	array = np.subtract(array, min_by_row)
+	max_by_row = np.max(array, axis = 1)
+	max_by_row = max_by_row.reshape(-1,1)
+	return np.divide(array, max_by_row)
+
+counts_df = pd.read_csv(counts_path)
+col_names = list(counts_df)
+gene_names = list(counts_df[col_names[0]]) #needed for labeling input features :)
+morph_features = [
+    'mean_diameter',
+    'max_branch_order',
+    'max_euclidean_distance',
+	'max_path_distance',
+	'num_outer_bifurcations',
+	'num_branches',
+	'num_nodes',
+	'num_tips',
+	'total_length',
+	'total_surface_area',
+	'total_volume']
+efel_features_col_names = [
+	'steady_state_voltage', 'steady_state_voltage_stimend',
+	'time_to_first_spike', 'time_to_last_spike',
+	'spike_count', 'AP_height', 'AP_width',
+	'sag_amplitude', 'sag_ratio1', 'sag_time_constant']
+
+del col_names, counts_df
+gc.collect()
 
 for stim_current in currents_to_check:
 	
@@ -45,6 +83,13 @@ for stim_current in currents_to_check:
 			
 	mean_volt_changes = np.asarray(mean_volt_changes)		
 	valid_inputs = np.asarray(valid_inputs)
+	
+	#we want to normalize the morphology data between all cells
+	#but we want to compare relative levels of gene expression within a given cell
+	#so we normalize based on the column (all cells) for the morphology and row for the transcriptome
+	morph_end = len(morph_features)
+	valid_inputs[:, :morph_end] = norm_col(valid_inputs[:, :morph_end])
+	valid_inputs[:, morph_end:] = norm_row(valid_inputs[:, morph_end:])
 		
 	mutual_info = mir(valid_inputs, mean_volt_changes, n_jobs=10)
 	print(mutual_info.shape)
